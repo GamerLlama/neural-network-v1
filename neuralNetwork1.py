04dc2ce5d3d8ddcf03d233590120ac6728835547
GamerLlama <53287152+GamerLlama@users.noreply.github.com>
2019-08-05 23:36:09 -0400

    Add files via upload

--- neuralNetwork1.py
+++ neuralNetwork1.py
@@ -1,1 +1,8 @@
-class NeuralNetwork
+class Node:
+    def __init__(self):
+        print("you just made a node, good job")
+
+class NeuralNetwork:
+    def __init__(self):
+        print("you just made the network, good job")
+
