a61b4841136ca40e1f622557d564e753a62b6cb5
GamerLlama <53287152+GamerLlama@users.noreply.github.com>
2019-08-06 00:59:29 -0400

    Add files via upload
    There's also gonna be some Weight objects, defined after the Nodes are

--- neuralNetwork1.py
+++ neuralNetwork1.py
@@ -1,8 +1,36 @@
+class Weight:
+    def __init__(self, startNode, endNode):
+        self.startNode = startNode
+        self.endNode = endNode
+        self.weight = 0
+    
+    def setWeight(self, value):
+        self.weight = value
+
 class Node:
-    def __init__(self):
-        print("you just made a node, good job")
+    def __init__(self, layerType, startValue):
+        self.layerType = layerType
+        self.value = startValue
+        if layerType=="input":
+            pass
+        elif layerType=="output":
+            pass
+        elif layerType=="hidden":
+            pass
+    
+    def setWeights(self, *args):
+        if layerType=="input" or layerType=="hidden":
+            self.outputWeights = args[-1]
+        
+        if layerType=="hidden" or layerType=="output":
+            self.inputWeights = args[0]
 
 class NeuralNetwork:
     def __init__(self):
-        print("you just made the network, good job")
+        pass
 
+    def setInputs(self, newInputs):
+        self.inputs = newInputs
+
+network = NeuralNetwork()
+network.test2()
