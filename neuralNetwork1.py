64e991db2d278f9851fbe3199d97eef249a9ab32
GamerLlama <53287152+GamerLlama@users.noreply.github.com>
2019-08-07 17:54:58 -0400

    Add files via upload
    I now have backpropagation, but there's an issue somewhere because it always trends to either 0 or 1 as a constant output

--- neuralNetwork1.py
+++ neuralNetwork1.py
@@ -26,9 +26,6 @@ class Weight:
         self.endNode.addInput(self)
         self.value = startValue
         self.ID = createNewID()
-    
-    def setValue(self, value):
-        self.value = value
 
 class Node:
     def __init__(self, layerType, startValue, startBias):
@@ -38,6 +35,8 @@ class Node:
         self.inputWeights = []
         self.outputWeights = []
         self.ID = createNewID()
+        self.error = 0
+        self.delta = 0
     
     def addInput(self, weight):
         self.inputWeights.append(weight)
@@ -46,11 +45,12 @@ class Node:
         self.outputWeights.append(weight)
 
 class NeuralNetwork:
-    def __init__(self, shape):
+    def __init__(self, shape, learningRate):
         self.layerSizes = shape
         self.createNodes()
         self.createWeights()
         self.trainingData = []
+        self.learningRate = learningRate
 
     def clearTrainingData(self):
         self.trainingData = []
@@ -121,12 +121,45 @@ class NeuralNetwork:
             self.setInputs(data[0])
             self.setCorrectOutputs(data[1])
             outputs = self.runExample()
+            #print(outputs)
             error = [0.5*(outputs[index]-data[1][index])**2 for index in range(self.layerSizes[-1])]
+            #print(error)
             totalError = [totalError[index] + error[index] for index in range(self.layerSizes[-1])]
-        print(totalError)
+        totalError = [totalError[index] / len(self.trainingData) for index in range(len(totalError))]
+        for index in range(len(totalError)):
+            outputNode = self.nodeLayers[-1][index]
+            outputNode.error = outputs[index]-data[1][index]
+            outputNode.delta = outputNode.value * (1-outputNode.value) * outputNode.error #sigmoid specific
+        return totalError
     
-networkShape = [2, 5, 1]
+    def generateHiddenError(self, layerIndex):
+        for index in range(self.layerSizes[layerIndex]):
+            startNode = self.nodeLayers[layerIndex][index]
+            startNode.error = sum([weight.value*weight.endNode.delta for weight in startNode.outputWeights])
+            startNode.delta = startNode.value * (1-startNode.value) * startNode.error
+    
+    def updateWeightsAndBiases(self):
+        for layer in self.weightLayers:
+            for weight in layer:
+                weight.value += weight.endNode.delta * weight.startNode.value * self.learningRate
+                #print(weight.value)
+        for layer in self.nodeLayers[1:]:
+            for node in layer:
+                node.bias += node.delta * self.learningRate
+                #print(node.bias)
+                #pass
+    
+    def train(self):
+        error = self.generateOutputError()
+        print("Error:", error)
+        for index in range(1, len(self.layerSizes)-1):
+            self.generateHiddenError(index)
+        self.updateWeightsAndBiases()
+        self.resetNodes()
+    
+networkShape = [2, 3, 1]
 trainingData = [[[0, 0], [0]], [[0, 1], [1]], [[1, 0], [1]], [[1, 1], [0]]]
-network = NeuralNetwork(networkShape)
+network = NeuralNetwork(networkShape, 0.5)
 network.addTrainingData(trainingData)
-network.generateOutputError()
+while True:
+    network.train()
